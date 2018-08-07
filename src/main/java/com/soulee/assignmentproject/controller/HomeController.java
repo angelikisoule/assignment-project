a83639e668a837ed2fb75d45bd61e38adf9eadf7
angelikisoule <angelikisoule@gmail.com>
2018-08-08 00:43:22 +0300

    ADD: angular

--- src/main/java/com/soulee/assignmentproject/controller/HomeController.java
+++ src/main/java/com/soulee/assignmentproject/controller/HomeController.java
@@ -1,21 +1,12 @@
 package com.soulee.assignmentproject.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class HomeController {
-
-
-	@RequestMapping(value = "/name/first")
-	public String getStudent() {
-		return "angeliki";
+	@RequestMapping("/home")
+	public String home() {
+		return "index";
 	}
-
-	@RequestMapping(value = "/name/last")
-	public String getLast() {
-		return "soule";
-	}
-
 }
